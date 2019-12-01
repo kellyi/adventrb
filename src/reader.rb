99bf442dda5009d6d466df4bb54aed60715f87d6
Kelly Innes <kinnes@gmail.com>
2019-12-01 10:46:18 -0500

    Update Reader module

--- src/reader.rb
+++ src/reader.rb
@@ -1,7 +1,8 @@
 # A module for reading data files
 module Reader
-  def self.read_data_from_file(filename)
+  def self.read_data_from_file(filename, custom_split_char = nil)
     filename_with_path = "./data/#{filename}.txt"
-    File.exist?(filename_with_path) && File.open(filename_with_path).read.split
+    split_args = custom_split_char ? [:split, custom_split_char] : [:split]
+    File.exist?(filename_with_path) && File.open(filename_with_path).read.send(*split_args)
   end
 end
